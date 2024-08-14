88d01a63be68512dcdfdde55f99ede6f45108a33
Ricardina Ngovene <ricardinangovene27@gmail.com>
2024-08-14 16:35:39 +0200

    add updateAluno

--- controller/aluno-controller.js
+++ controller/aluno-controller.js
@@ -48,10 +48,31 @@ const getAlunoById = async (req, res) => {
     res.status(500).json({ message: 'Erro ao buscar aluno', error: err.message });
   }
 };
+const updateAluno = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const atualizacao = req.body;
+    const aluno = await Aluno.findByIdAndUpdate(id, atualizacao, { new: true }).populate('idTurma');
+    if (!aluno) {
+      return res.status(404).json({ message: 'Aluno não encontrado' });
+    }
+    res.status(200).json(aluno);
+  } catch (err) {
+    res.status(400).json({ message: 'Erro ao atualizar aluno', error: err.message });
+  }
+};
+const deleteAluno = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const resultado = await Aluno.findByIdAndDelete(id);
+    if (!resultado) {
+      return res.status(404).json({ message: 'Aluno não encontrado' });
+    }
+    res.status(200).json({ message: 'Aluno excluído com sucesso' });
+  } catch (err) {
+    res.status(500).json({ message: 'Erro ao excluir aluno', error: err.message });
+  }
+};
 
 
-
-
-
-
-module.exports ={createAluno,getAlunos,getAlunoById};
+module.exports ={createAluno,getAlunos,getAlunoById, updateAluno, deleteAluno};
