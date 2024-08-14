5593a6906c93a65c6bd200c131586826bddaf0a1
yuran-nhassengo <yurannhassengo@gmail.com>
2024-08-14 17:21:47 +0200

    Add notas-controller

---
+++ controller/nota-controller.js
@@ -0,0 +1,54 @@
+const Notas = require('../models/notas');
+
+
+const createNota = async (req, res) => {
+  try {
+    const { nota} = req.body;
+
+    const turmaId =  req.aluno.id; 
+
+
+    if (!nota) {
+        return res
+          .status(400)
+          .json({ message: "Todos os campos sao Obrigatorios." });
+      }
+
+    const novaNota = await Notas.create({
+         nota,
+        idAluno:turmaId,
+        idAvaliacao:turmaId });
+    
+    res.status(201).json(resultado);
+  } catch (err) {
+    res.status(400).json({ message: 'Erro ao criar nota', error: err.message });
+  }
+};
+
+
+const getNotas = async (req, res) => {
+  try {
+    const notas = await Notas.find().populate('idAluno').populate('idAvaliacao'); // Inclui dados referenciados
+    res.status(200).json(notas);
+  } catch (err) {
+    res.status(500).json({ message: 'Erro ao buscar notas', error: err.message });
+  }
+};
+
+const getNotaById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const nota = await Notas.findById(id).populate('idAluno').populate('idAvaliacao');
+    if (!nota) {
+      return res.status(404).json({ message: 'Nota não encontrada' });
+    }
+    res.status(200).json(nota);
+  } catch (err) {
+    res.status(500).json({ message: 'Erro ao buscar nota', error: err.message });
+  }
+};
+
+
+
+
+module.exports ={createNota,getNotas,getNotaById}
---
+++ model/nota-model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+
+const notasSchema = new mongoose.Schema({
+  nota: {
+    type: Number,
+    required: true, 
+    min: 0, 
+    max: 10 
+  },
+  idAluno: {
+   // type: mongoose.Schema.Types.ObjectId,
+    //ref: 'Aluno', 
+    type: Number,
+    required: true,
+  },
+  idAvaliacao: {
+    //type: mongoose.Schema.Types.ObjectId,
+    //ref: 'Avaliacao', 
+    type: Number,
+    required: true, 
+  },
+});
+
+const Notas = mongoose.model('Notas', notasSchema);
+
+module.exports = Notas;
