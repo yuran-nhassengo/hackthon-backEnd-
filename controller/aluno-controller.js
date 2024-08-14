6df856d782166c469ec1b3ac9e09271f6fc56b2f
yuran-nhassengo <yurannhassengo@gmail.com>
2024-08-14 16:32:49 +0200

    add create and get aluno controller

---
+++ controller/aluno-controller.js
@@ -0,0 +1,57 @@
+const asyncHandler = require("express-async-handler");
+
+const Aluno = require('../model/aluno-model');
+
+
+const createAluno = asyncHandler(async (req, res) => {
+    try {
+      const { nome, dataNascimento, sexo, idTurma } = req.body;
+
+      if (!nome || !dataNascimento || !sexo || !idTurma) {
+        return res
+          .status(400)
+          .json({ message: "Todos os campos sao Obrigatorios." });
+      }
+  
+      const novoAluno = await Aluno.create({ 
+        nome, 
+        dataNascimento,
+         sexo,
+        idTurma });
+  
+      res.status(201).json( {message: "Aluno criado com sucesso!",resultado});
+    } catch (err) {
+      res.status(400).json({ message: 'Erro ao criar aluno', error: err.message });
+    }
+  });
+
+
+const getAlunos = async (req, res) => {
+  try {
+    const alunos = await Aluno.find().populate('idTurma'); 
+    res.status(200).json(alunos);
+  } catch (err) {
+    res.status(500).json({ message: 'Erro ao buscar alunos', error: err.message });
+  }
+};
+
+
+const getAlunoById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const aluno = await Aluno.findById(id).populate('idTurma');
+    if (!aluno) {
+      return res.status(404).json({ message: 'Aluno não encontrado' });
+    }
+    res.status(200).json(aluno);
+  } catch (err) {
+    res.status(500).json({ message: 'Erro ao buscar aluno', error: err.message });
+  }
+};
+
+
+
+
+
+
+module.exports ={createAluno,getAlunos,getAlunoById};
---
+++ model/aluno-model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+
+const alunoSchema = new mongoose.Schema({
+  nome: {
+    type: String,
+    required: true, 
+  },
+  dataNascimento: {
+    type: Date,
+    required: true, 
+  },
+  sexo: {
+    type: String,
+    enum: ['Masculino', 'Feminino', 'Outros'], 
+    required: true, 
+  },
+  idTurma: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Turma', 
+    required: true, 
+  },
+});
+
+
+const Aluno = mongoose.model('Aluno', alunoSchema);
+
+module.exports = Aluno;
